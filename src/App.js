e8c671a6445529e6cdd9aa509f588d451f3a7844
llcain-github <llcain2012@gmail.com>
2021-08-22 03:36:51 -0500

    react prayground button hover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="banner">
+        <div className="navbar">
+          <img src="/buttonhover/src/assets/images/blue-logo.png" alt="Blue logo" className="logo"/>
+          <ul>
+            <li><a href="home.js">Home</a></li>
+            <li><a href="bedroom.js">Bedroom</a></li>
+            <li><a href="dining.js">Dining</a></li>
+            <li><a href="kitchen.js">Kitchen</a></li>
+            <li><a href="backyard.js">Backyard</a></li>
+          </ul>
+        </div>
+
+        <div className="content">
+          <h1>DESIGN YOUR HOUSE</h1>
+          <p>Subscribe Easy Tutorials YouTub Channel to watch more videos, <br/>press the bell icon to get latest updates.</p>
+          <div>
+            <button type="button"><span></span>WATCH MORE</button>
+            <button type="button"><span></span>SUBSCRIBE</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
